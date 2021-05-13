e7e19ee3375125267640d8b729ec53f485bbd4c8
Anasheh-Mirzakhanian <81529489+Anasheh-Mirzakhanian@users.noreply.github.com>
2021-05-13 20:57:56 +0430

    Add files via upload

---
+++ Text_Editor/Wysiwyg/Scripts/BaseButtons/baseButton.js
@@ -0,0 +1,10 @@
+﻿class BaseButton extends BaseCustomControl {
+    constructor({ element, container, attrs, cap }) {
+        super({
+            element: element,
+            container: container,
+            attrs: attrs,
+            cap: cap
+        })
+    }
+}
