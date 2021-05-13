fb8d53451dd26178fa374503a1b67cebc18a1c8b
Anasheh-Mirzakhanian <81529489+Anasheh-Mirzakhanian@users.noreply.github.com>
2021-05-13 20:24:35 +0430

    Add files via upload

---
+++ Text_Editor/Wysiwyg/App_Start/BundleConfig.cs
@@ -0,0 +1,50 @@
+﻿using System.Web.Optimization;
+
+namespace Wysiwyg
+{
+    public class BundleConfig
+    {
+        // For more information on bundling, visit https://go.microsoft.com/fwlink/?LinkId=301862
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new ScriptBundle("~/bundles/script/js").Include(
+                        "~/Scripts/baseCustomControl.js",
+                        "~/Scripts/wysiwyg.js",                       
+                         "~/Scripts/BaseBars/baseBar.js",
+                         "~/Scripts/BaseBars/toolBar.js",                                 
+                         "~/Scripts/BaseButtons/baseButton.js",
+                         "~/Scripts/BaseButtons/ToolButtons/toolButton.js",
+                         "~/Scripts/BaseButtons/ToolButtons/boldButton.js",
+                         "~/Scripts/BaseButtons/ToolButtons/italicButton.js",
+                         "~/Scripts/BaseButtons/ToolButtons/underlineButton.js",
+                         "~/Scripts/BaseButtons/ToolButtons/strikeThroughButton.js",
+                         "~/Scripts/BaseButtons/ToolButtons/subScriptButton.js",
+                         "~/Scripts/BaseButtons/ToolButtons/superScriptButton.js",
+                         "~/Scripts/BaseButtons/ToolButtons/highlightColorInputButton.js",
+                         "~/Scripts/BaseButtons/ToolButtons/highlightColorButton.js",
+                         "~/Scripts/BaseButtons/ToolButtons/fontColorInputButton.js",
+                         "~/Scripts/BaseButtons/ToolButtons/fontColorButton.js",
+                         "~/Scripts/BaseButtons/ToolButtons/alignLeftButton.js",
+                         "~/Scripts/BaseButtons/ToolButtons/alignCenterButton.js",
+                         "~/Scripts/BaseButtons/ToolButtons/alignRightButton.js",
+                         "~/Scripts/BaseButtons/ToolButtons/redoButton.js",
+                         "~/Scripts/BaseButtons/ToolButtons/undoButton.js",
+                         "~/Scripts/editor.js",
+                         "~/Scripts/BaseBars/fileBar.js",
+                         "~/Scripts/fileDiv.js",
+                         "~/Scripts/fileInput.js",
+                         "~/Scripts/fileLabel.js",
+                         "~/Scripts/fileSpan.js",
+                         "~/Scripts/BaseBars/buttonBar.js",
+                         "~/Scripts/BaseButtons/ActionButtons/actionButton.js",
+                         "~/Scripts/BaseButtons/ActionButtons/newButton.js",
+                         "~/Scripts/BaseButtons/ActionButtons/saveButton.js",
+                         "~/Scripts/BaseButtons/ActionButtons/deleteButton.js"
+                        ));;
+
+            bundles.Add(new StyleBundle("~/Content/css").Include(
+                      "~/Content/style/wysiwyg.css"
+                      ));
+        }
+    }
+}
---
+++ Text_Editor/Wysiwyg/App_Start/FilterConfig.cs
@@ -0,0 +1,12 @@
+﻿using System.Web.Mvc;
+
+namespace Wysiwyg
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
